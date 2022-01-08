6ed3aa5746f9ee409746a369697dfe15baf1fa53
sxv20_ <sebastian.venter@gmail.com>
2022-01-08 17:21:43 +0000

    alpha-beta, quiescence search

--- src/bitboards/search.rs
+++ src/bitboards/search.rs
@@ -2,27 +2,50 @@ use super::*;
 
 impl BitBoards {
     pub fn search(&self) -> (i32, Move) {
-        self.negamax(5)
+        self.negamax(i32::MIN + 1, i32::MAX - 1, 7)
     }
 
-    fn negamax(&self, depth: usize) -> (i32, Move) {
+    fn negamax(&self, mut alpha: i32, beta: i32, depth: usize) -> (i32, Move) {
         if depth == 0 {
-            return (self.evaluate(), Move::null())
+            return (self.quiesce(alpha, beta), Move::null());
         }
 
         let moves = self.legal_moves();
 
-        let mut best_score = i32::MIN;
         let mut best_move = Move::null();
         for move_ in moves {
             let mut copy = self.clone();
             copy.make_move(move_);
-            let score = -copy.negamax(depth - 1).0;
-            if score > best_score {
-                best_score = score;
+            let score = -copy.negamax(-beta, -alpha, depth - 1).0;
+            if score >= beta {
+                return (beta, move_);
+            }
+            if score > alpha {
+                alpha = score;
                 best_move = move_;
             }
         }
-        (best_score, best_move)
-    }   
+        (alpha, best_move)
+    }
+
+    pub fn quiesce(&self, mut alpha: i32, beta: i32) -> i32 {
+        let stand_pat_score = self.evaluate();
+
+        if stand_pat_score >= beta {
+            return beta;
+        }
+        alpha = alpha.max(stand_pat_score);
+
+        let moves = self.legal_moves();
+        for move_ in moves.iter().filter(|m| m.capture()) {
+            let mut boards = self.clone();
+            boards.make_move(*move_);
+            let score = -boards.quiesce(-beta, -alpha);
+            if score >= beta {
+                return beta;
+            }
+            alpha = alpha.max(score);
+        }
+        alpha
+    }
 }
