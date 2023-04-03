7dc8683e952d0c79d133155325b5f2d3cd2882f4
Algorhythm-sxv <sebastian.venter@gmail.com>
2023-04-03 19:55:25 +0200

    remove all move ordering except TT. bench: 2201924

--- cheers_lib/src/move_sorting.rs
+++ cheers_lib/src/move_sorting.rs
@@ -107,24 +107,7 @@ impl<M: TypeMoveGen> MoveSorter<M> {
 }
 
 fn score_capture(board: &Board, mv: Move) -> i32 {
-    // filter out underpromotions
-    if matches!(mv.promotion, Knight | Bishop | Rook) {
-        return UNDERPROMO_SCORE + (SEE_PIECE_VALUES[mv.promotion] as i32);
-    }
-    let mvv_lva = if mv.promotion == Queen {
-        MVV_LVA[Queen][Pawn]
-    } else {
-        MVV_LVA[board.piece_on(mv.to).unwrap_or(Pawn)][mv.piece]
-    };
-    let relative_square = if board.current_player() == Color::White {
-        relative_board_index::<White>(mv.to)
-    } else {
-        relative_board_index::<Black>(mv.to)
-    };
-    let psqt_score = EVAL_PARAMS.piece_tables[(mv.piece, relative_square)].mg() as i32 / 16;
-
-    // sort all captures before quiets
-    WINNING_CAPTURE_SCORE + 1000 * (mvv_lva as i32) + psqt_score
+    0
 }
 
 fn score_quiet(
@@ -135,13 +118,5 @@ fn score_quiet(
     last_move: Move,
     mv: Move,
 ) -> i32 {
-    let current_player = board.current_player();
-    if killers.contains(&mv) {
-        // there can be more than 1 killer move, so sort them by their respective histories
-        KILLER_MOVE_SCORE + (history[current_player][mv.piece][mv.to] as i32)
-    } else if counters[current_player][last_move.piece][last_move.to] == mv {
-        COUNTERMOVE_SCORE
-    } else {
-        QUIET_SCORE + (history[current_player][mv.piece][mv.to] as i32)
-    }
+    0
 }
