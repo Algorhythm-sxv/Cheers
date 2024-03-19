635ca25774e047fbc915e5d38c04154ec96ce43d
Algorhythm-sxv <sebastian.venter@gmail.com>
2024-03-19 10:34:31 +0100

    never reduce to zero
    bench: 1097650

--- cheers_lib/src/search.rs
+++ cheers_lib/src/search.rs
@@ -540,7 +540,7 @@ impl Search {
                     &new,
                     -beta,
                     -beta + 1,
-                    (depth - reduction).max(0),
+                    (depth - reduction).max(1),
                     ply + 1,
                     &mut line,
                     tt,
@@ -671,7 +671,7 @@ impl Search {
                 };
 
                 // perform a cheap reduced, null-window search in the hope it fails low immediately
-                let reduced_depth = (depth - 1 - reduction).max(0);
+                let reduced_depth = (depth - 1 - reduction).max(1);
                 score = -self.negamax::<NotRoot, M>(
                     &new,
                     -alpha - 1,
